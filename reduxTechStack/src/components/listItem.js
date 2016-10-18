344d8c0cadcf474a9899f94a1a5d1715f6084b3e
Robert Skwiat <robert@mediabrix.com>
2016-10-18 10:44:59 -0400

    working with redux basic stack and animation

---
+++ reduxTechStack/src/components/listItem.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+
+  componentWillUpdate() {
+    LayoutAnimation.easeInEaseOut();
+  }
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={styles.descriptionStyle}>{library.description}</Text>
+        </CardSection>
+      );
+    }
+  }
+
+  render() {
+    const { id, title } = this.props.library;
+
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>{title}</Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  },
+  descriptionStyle: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    lineHeight: 18
+  }
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
